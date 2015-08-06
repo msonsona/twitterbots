cf9f442412df18223b1902e2a340bbaf40e86ade
Miquel Sonsona <miquelsonsona@gmail.com>
2015-08-06 15:44:05 +0200

    credentials file absolute path
    Script wasn’t finding the pickled credentials file

--- credentials_unpickler.py
+++ credentials_unpickler.py
@@ -1,9 +1,11 @@
+import os
 import pickle
 
 def unpickle(account):
-    filename = "%s.pickle" % account
-    
+    filename = os.path.dirname(os.path.realpath(__file__))
+    filename += "/%s.pickle" % account
+
     with open(filename, 'rb') as f:
         credentials = pickle.load(f)
-    
+
     return credentials
