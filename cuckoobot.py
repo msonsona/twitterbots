a5812cf557df174fb42f54b886387029ac21d1fe
Miquel Sonsona <miquelsonsona@gmail.com>
2015-08-06 07:44:04 +0200

    add clockface, tweet and credential loading from file

--- cuckoobot.py
+++ cuckoobot.py
@@ -1,17 +1,51 @@
 #!/usr/bin/python3
+from sys import argv
 from datetime import datetime
+import credentials_unpickler
+import tweepy
+
+script, account = argv
+credentials = credentials_unpickler.unpickle(account)
 
 now = datetime.utcnow()
 
-hour = now.hour
+hour = now.hour % 12
 
-hour = hour % 12
-if (hour == 0):
+tweet = "It's "
+if hour == 0:
     hour = 12
+    tweet += "🕛"
+elif hour == 1:
+    tweet += "🕐"
+elif hour == 2:
+    tweet += "🕑"
+elif hour == 3:
+    tweet += "🕒"
+elif hour == 4:
+    tweet += "🕓"
+elif hour == 5:
+    tweet += "🕔"
+elif hour == 6:
+    tweet += "🕕"
+elif hour == 7:
+    tweet += "🕖"
+elif hour == 8:
+    tweet += "🕗"
+elif hour == 9:
+    tweet += "🕘"
+elif hour == 10:
+    tweet += "🕙"
+elif hour == 11:
+    tweet += "🕚"
 
-sound = ""
+tweet += "! "
+
 for i in range(hour):
-    sound += "🐤cuckoo "
+    tweet += "🐤cuckoo "
 
-tweet = sound
-print(tweet)
+auth = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_secret'])
+auth.set_access_token(credentials['access_token'], credentials['access_token_secret'])
+
+api = tweepy.API(auth)
+
+api.update_status(status=tweet)
