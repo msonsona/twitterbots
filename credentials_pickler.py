ff3214256a20c7c823b909a181658f2182e42051
Miquel Sonsona <miquelsonsona@gmail.com>
2015-11-06 07:57:57 +0100

    ability to store extra info in credentials pickled files
    Extra key-value pairs can be stored in the credentials files

--- credentials_pickler.py
+++ credentials_pickler.py
@@ -7,6 +7,17 @@ consumer_secret = input("3. Enter the consumer_secret > ")
 access_token = input("4. Enter the access_token > ")
 access_token_secret = input("5. Enter the access_token_secret > ")
 
+extra_info = 'y'
+extra_tokens = []
+while extra_info == 'y':
+    extra_info = input("Do you want to store any extra key - value pair? (y/n) > ")
+    if extra_info == 'y':
+        extra_info_key = input("First, enter the name (key) > ")
+        extra_info_value = input("Second, enter the value to store > ")
+        extra_tokens.append({'key': extra_info_key,
+                             'value': extra_info_value})
+    
+
 credentials = {
     'account': account,
     'consumer_key': consumer_key,
@@ -14,6 +25,9 @@ credentials = {
     'access_token': access_token,
     'access_token_secret': access_token_secret
 }
+
+if len(extra_tokens):
+    credentials['extra_tokens'] = extra_tokens
 
 print(credentials)
 
