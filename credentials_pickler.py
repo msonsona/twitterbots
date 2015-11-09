f385351611d66502fe7fe765cb9446fd87a52bbc
Miquel Sonsona <miquelsonsona@gmail.com>
2015-11-09 08:10:42 +0100

    extra_tokens is a key-value dict

--- credentials_pickler.py
+++ credentials_pickler.py
@@ -8,14 +8,13 @@ access_token = input("4. Enter the access_token > ")
 access_token_secret = input("5. Enter the access_token_secret > ")
 
 extra_info = 'y'
-extra_tokens = []
+extra_tokens = {}
 while extra_info == 'y':
     extra_info = input("Do you want to store any extra key - value pair? (y/n) > ")
     if extra_info == 'y':
         extra_info_key = input("First, enter the name (key) > ")
         extra_info_value = input("Second, enter the value to store > ")
-        extra_tokens.append({'key': extra_info_key,
-                             'value': extra_info_value})
+        extra_tokens[extra_info_key] = extra_info_value
     
 
 credentials = {
