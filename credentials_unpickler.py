706ec90f0781cf5835bbd78e2001013fcf98436a
Miquel Sonsona <miquelsonsona@gmail.com>
2015-12-07 14:45:09 +0100

    pick credentials files from a specific directory

--- credentials_unpickler.py
+++ credentials_unpickler.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
 import os
 import pickle
 
 def unpickle(account):
     filename = os.path.dirname(os.path.realpath(__file__))
-    filename += "/%s.pickle" % account
-
+    filename = "{}/credentials/{}.pickle".format(filename, account)
+    
     with open(filename, 'rb') as f:
         credentials = pickle.load(f)
-
+    
     return credentials
