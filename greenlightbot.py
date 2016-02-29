cb3f58c96f6d05e3fe2330d04db3f1a0642f1d61
Miquel Sonsona <miquelsonsona@gmail.com>
2016-02-29 03:58:37 +0100

    tweeting and notifying

--- greenlightbot.py
+++ greenlightbot.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-import credentials_unpickler
+from sys import argv
+from datetime import date, timedelta
+import time
+
 import tweepy
+
+import credentials_unpickler
+
 
 def insert_in_top10(top10, position, element):
     # 1- insert into corresponding position
@@ -8,31 +14,100 @@ def insert_in_top10(top10, position, element):
     # 2- deleting last element of top10
     del(top10[-1])
 
+start = time.time()
+
+script, account = argv
+credentials = credentials_unpickler.unpickle(account)
+
+auth = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_secret'])
+auth.set_access_token(credentials['access_token'], credentials['access_token_secret'])
+
+api = tweepy.API(auth)
+
 q = ['#screenshotsaturday', '#mobilegames', '#gamedev', '#madewithunity', '#ue4', '#indiedev', '#pixelart']
-w = date.today().weekday()
 
-print("Today is ", date.today(), " | weekday ", date.today().weekday())
-print("So I'll query for", q[w])
+today = date.today()
+weekday = today.weekday()
+week = timedelta(days=7)
 
-top10 = [{'id':0, 'likes':-1} for i in range(10)]
+print("Today is ", today, " | weekday ", weekday)
+print("So I'll query for", q[weekday])
 
-for page in tweepy.Cursor(api.search, q=q[w], since='2016-02-20', until='2016-02-21').pages(5):
+top10 = [{'id':0, 'likes':-1, 'user_name':''} for i in range(10)]
+
+# Sort tweets for the given keyword by likes, keep only the top 10
+num_tweets = 0
+for page in tweepy.Cursor(api.search, q=q[weekday], since=today-week, count=100).pages():
     for tweet in page:
-        # pprint(tweet._json)
-        print(tweet.id, tweet.favorite_count)
+        num_tweets += 1
+        # print(tweet.id, tweet.favorite_count)
         if tweet.favorite_count >= top10[0]['likes']:
-            insert_in_top10(top10, 0, {'id':tweet.id,
-                                       'likes':tweet.favorite_count})
+            insert_in_top10(top10, 
+                            0, 
+                            {'id':tweet.id,
+                             'likes':tweet.favorite_count,
+                             'user_name':tweet.user.screen_name})
         elif tweet.favorite_count <= top10[-1]['likes']:
             pass
         else:
-            print("sorting")
-            for i in range(9,-1,-1):
+            # print("sorting")
+            for i in range(9, -1, -1):
                 if top10[i]['likes'] > tweet.favorite_count:
-                    insert_in_top10(top10, i+1, {'id':tweet.id,
-                                                 'likes':tweet.favorite_count})
+                    insert_in_top10(top10, 
+                                    i+1, 
+                                    {'id':tweet.id,
+                                     'likes':tweet.favorite_count,
+                                     'user_name':tweet.user.screen_name})
                     break
-print(top10)
+    print(num_tweets)
 
 for t in top10:
     print(t)
+
+e = ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟']
+top = 0
+if top10[9]['likes'] > 0:
+    # Top10
+    top = 10
+elif top10[4]['likes'] > 0:
+    # Top5
+    top = 5
+elif top10[2]['likes'] > 0:
+    # Top3
+    top = 3
+else:
+    # not enough likes !?
+    pass
+    
+if top:
+    ok_status = "{} Top{} on {}".format(q[weekday], 
+                                        top, 
+                                        date.today())
+    original_status = api.update_status(status=ok_status)
+    print(original_status.text)
+    original_status_id = original_status.id
+
+    for i in range(top-1, -1, -1):
+        text = "🔝{} https://twitter.com/{}/status/{}"
+        tweet = text.format(e[i], top10[i]['user_name'], top10[i]['id'])
+        reply = api.update_status(status=tweet,
+                                  in_reply_to_status_id=original_status_id)
+        # keep id for iteration
+        original_status_id = reply.id
+        time.sleep(10)
+
+# Summary notification
+status_update = "@msonsona "
+if top:
+    status_update += "top{} ".format(top)
+else:
+    status_update += "💩 not enough likes "
+
+status_update += "for {} on {}, {} tweets ranked in {} secs".format(
+    q[weekday], 
+    date.today(), 
+    num_tweets, 
+    round(time.time() - start, 2)
+    )
+api.update_status(status=status_update)
+print(status_update.text)
