e0ef351b713f874f3ca74124f24661137f44faf6
Miquel Sonsona <miquelsonsona@gmail.com>
2016-04-28 04:02:25 +0200

    breaking the loop when there aren't more tweets

--- greenlightbot.py
+++ greenlightbot.py
@@ -101,6 +101,8 @@ while tweets:
                                   auth=req_auth)
         search_json = search_req.json()
         tweets = search_json['statuses']
+    else:
+        tweets = None
 
 for t in top10:
     print(t)
